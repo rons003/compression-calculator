b405aafe31e09442190d3b7aa323dbead73899b6
rons003 <ronelle003@gmail.com>
2025-04-10 13:38:58 +0800

    project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbNavModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'compression-calculator';
+  active = 1;
+  cr: number = 0;
+
+  bore: number = 0;
+  stroke: number = 0;
+  uVolume: number = 0;
+
+  calculate(){
+    const sVolume = this.bore * this.bore * this.stroke * 0.0007854;
+    this.cr = (sVolume + this.uVolume) / this.uVolume;
+
+  }
+
+  getCompressionRatio(): string {
+    const cr = Math.round(this.cr);
+    return cr.toString() + " : 1";
+  }
 }
